b86509b0e45607d500a5b220e5509508b8101394
Anekdotin <eddwinn@gmail.com>
2019-04-20 16:28:54 -0400

    api needs polish. Test form

--- app/createapibot/get_users_api.py
+++ app/createapibot/get_users_api.py
@@ -1,24 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
+options = Options()
 import time
 
-from selenium import webdriver
-driver = webdriver.PhantomJS()
+options.headless = True
+driver = webdriver.Firefox(options=options, executable_path="/home/bot/reddit/venv/lib/python3.5/geckodriver")
 driver.set_window_size(1120, 550)
-driver.get('http://reddit.com/')
+driver.get('https://www.reddit.com/login?dest=https%3A%2F%2Fwww.reddit.com%2F')
 
-form = driver.find_element_by_id("login_login-main")
-username = form.find_element_by_name("user")
+# get the form
+form = driver.find_element_by_class_name("AnimatedForm")
+
+#login to form
+username = form.find_element_by_id("loginUsername")
 username.clear()
-username.send_keys("username")
+username.send_keys("Mirbekov46")
 
-password = form.find_element_by_name("passwd")
+password = form.find_element_by_id("loginPassword")
 password.clear()
-password.send_keys("password")
+password.send_keys("BE4M1HJ63T01VBK8IJKRILMDX2")
 
-submit = form.find_element_by_xpath("//button[. = 'login']")
+submit = form.find_element_by_class_name("AnimatedForm__submitButton")
 submit.click()
+time.sleep(2)
+
+
+
+# go back to reddit
+driver.get('https://www.reddit.com/prefs/apps')
 
 print(driver.current_url)
+time.sleep(4)
 
-# Close the browser!
-# driver.quit()
+driver.save_screenshot("1231.png")
+
+form_button = driver.find_element_by_class_name("edit-app-form")
+form_button.click()
+
+time.sleep(2)
+
+driver.save_screenshot("12312.png")
+
+
+
+#fill out the form
+y = driver.find_element_by_name("name").is_enabled()
+print(y)
+if y is True:
+    gettable = driver.find_element_by_class_name("preftable")
+
+    nameinput = gettable.find_element_by_name("name")
+    nameinput.send_keys("firstapp")
+    descripinput = gettable.find_element_by_name("description")
+    descripinput.send_keys("for science")
+else:
+    print("not enabled")
+driver.quit()
+
+
