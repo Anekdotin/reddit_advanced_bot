d67a80cace00f730f149b7aa0790e26b5bfddd5a
Anekdotin <eddwinn@gmail.com>
2019-05-04 23:45:06 -0400

    adding to db success. working api creator

--- app/createapibot/get_users_api.py
+++ app/createapibot/get_users_api.py
@@ -43,9 +43,8 @@ def main():
     theredirection = prawsite
 
 
-
-
     print("1")
+
     ## basic selenium
     options = Options()
     options.headless = True
@@ -60,7 +59,6 @@ def main():
     except Exception as e:
         print(str(e))
         driver.save_screenshot('error-pic-connection.png')
-    print("2")
 
 
     ## query db and get users without an api
@@ -83,8 +81,6 @@ def main():
     except Exception as e:
         print(str(e))
         driver.save_screenshot('error-pic-loginform.png')
-    print("3")
-
 
 
     try:
@@ -96,27 +92,20 @@ def main():
         driver.implicitly_wait(5)
         ActionChains(driver).move_to_element(form_button).click(form_button)
         time.sleep(2)
-        driver.save_screenshot("pic-01.png")
     except Exception as e:
         print(str(e))
         driver.save_screenshot('error-pic-editbutton.png')
-    print("4")
-
 
 
     ## FILL OUT APP
     #fill out the form
     try:
         get_creeate_app_button = driver.find_element_by_xpath('//*[@id="create-app-button"]')
-
-        print(get_creeate_app_button)
         get_creeate_app_button.click()
         time.sleep(2)
-        driver.save_screenshot("pic-011.png")
     except Exception as e:
         print(str(e))
         driver.save_screenshot('error-pic-createappbutton.png')
-    print("5")
 
 
     try:
@@ -133,7 +122,6 @@ def main():
         # check script button
         scriptbutton = gettable.find_element_by_id("app_type_script")
         scriptbutton.click()
-        driver.save_screenshot("pic-22.png")
         # about url
         aboutinput = gettable.find_element_by_name("about_url")
         aboutinput.send_keys(theabouturl)
@@ -141,34 +129,40 @@ def main():
         redirectinput = gettable.find_element_by_name("redirect_uri")
         redirectinput.send_keys(theredirection)
         #create app button
-        driver.save_screenshot("pic-33.png")
         thebutton = gettable.find_elements_by_xpath("//button[contains(text(), 'create app')]")
-        #thebutton = gettable.find_elements_by_xpath('/html/body/div[3]/div[2]/form/button')
         for f in thebutton:
             f.click()
-            print(f)
-        print(thebutton)
-        #thebutton.click()
+
     except Exception as e:
         print(str(e))
         driver.save_screenshot('error-pic-apiform.png')
-    print("6")
 
-
-
-    #after it saves..go back in and get our info
-    driver.save_screenshot("pic-4.png")
     ##SAVE TO DB
-    user.client_id = ""
-    user.client_secret = ""
-    user.user_agent = ""
-    user.appname = ""
+    try:
+        # secret
+        get_secret_text = driver.find_element_by_xpath('/html/body/div[3]/div[1]/ul/li/div[4]/div[1]/form/table/tbody/tr[1]/td')
+        thetextofsecret = get_secret_text.text
+        user.client_secret = str(thetextofsecret)
 
-    session2.add(user)
-    session2.commit()
+        # client id
+        get_id_ofapp = driver.find_element_by_xpath('/html/body/div[3]/div[1]/ul/li/div[2]/h3[2]')
+        get_id_ofapptext = get_id_ofapp.text
+        user.client_id = str(get_id_ofapptext)
 
+        # client agent is crawler and ai of username
+        user.user_agent = ""
 
+        user.appname = randomname
 
+        session2.add(user)
+        session2.commit()
+        print("api added")
+        print("user: ", user.username)
+        print("user secret: ", user.client_secret)
+        print("user id: ", user.client_id)
+    except Exception as e:
+        print(str(e))
+        driver.save_screenshot('error-get-secret-and-id.png')
 
     driver.quit()
 
